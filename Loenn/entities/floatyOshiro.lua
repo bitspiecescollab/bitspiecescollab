4cc27a26e82776083551199bf01f413485fe1c94
bitnetwork <11969018+bitnetwork@users.noreply.github.com>
2023-05-14 00:54:50 -0400

    build loenn scripts for *someone*

---
+++ Loenn/entities/floatyOshiro.lua
@@ -0,0 +1,26 @@
+return {
+  name = "BitsPieces/FloatyOshiro",
+  texture = "characters/oshiro/boss13.png",
+  -- nodeTexture = "blah2",
+  nodeLineRenderType = "line",
+  nodeLimits = {2, 2},
+  placements = {
+    name = "entity",
+    data = {
+      speed = 1.0,
+      flag = "",
+      recolor = "#FFFFFFFF",
+      texture = "oshiro_boss",
+      fadeSpeed = 0.75,
+      hitboxRadius = 14.0,
+      framesPerParticle = 3,
+      particleRadius = 10.0,
+      particleColor1 = "#44B7FF",
+      particleColor2 = "#75C9FF",
+      wobbleSpeed = 2.0,
+      wobbleAmplitude = 3.0,
+      enabled = true,
+      noLighting = false
+    }
+  }
+}
